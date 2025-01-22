61b98b0e6207b754ecf97eee19ea9e79d154930b
Nicoli Santos <Nicolisantosbatista@gmail.com>
2025-01-22 17:17:12 -0300

    fundamentos

---
+++ fundamentos/operadores.js
@@ -0,0 +1,35 @@
+/**
+ * OPERADORES
+ * COMPARAÇÃO
+ * LOGICOS
+ */
+
+console.clear()
+console.log("operadores aritméticos")
+console.log(" + | soma")
+console.log(" - | subtração")
+console.log(" * | multiplicação ")
+console.log(" / | divisão ")
+console.log(" % | módulo (resto da divisão)")
+console.log(" ")
+console.log(" operadores aritméticos de atribuição ")
+console.log(" += | soma e atribuiçao a variável ")
+console.log(" -=| subtração e atribuição a variável ")
+console.log(" *= | multiplicação e atribuição a variável")
+console.log(" /= | divisão e atribuição a variável ")
+console.log(" ++ | soma mais 1 ao resultado")
+console.log(" -- | subtrai 1 do resultado ")
+console.log(" ")
+console.log(" comparadores ")
+console.log(" > | maior que")
+console.log(" < | menor que ")
+console.log(" >= | maior ou igual ")
+console.log(" -> | menor ou igual ")
+console.log(" === | identico a ")
+console.log(" !== | não identico a ")
+
+console.log("  ")
+console.log(" operadores lógicos ")
+console.log(" && | AND ")
+console.log(" || | OR ")
+console.log(" ! | NOT ")
